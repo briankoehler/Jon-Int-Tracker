82e21290c18880e853acddeacae7f4d067d88819
briankoehler <briankoehler@ufl.edu>
2020-10-31 03:40:34 -0400

    Implement new multi-summoner feature.  Not yet tested

--- src/init.py
+++ src/init.py
@@ -4,29 +4,56 @@ import pickle
 
 
 class Summoner:
-    def __init__(self, name, encryptedId):
+    def __init__(self, id, name, encrypted_id, last_game_id):
+        self.id = id
         self.name = name
-        self.encryptedId = encryptedId
+        self.encrypted_id = encrypted_id
+        self.last_game_id = last_game_id
+
 
 def main():
+    summoners_names_list = []
+    summoners_list = []
+
     # Int Updates
     print('Thank you for using Jon Int Tracker (JIT).\nAnswers to the following questions will be to track inting of a user.')
     DISCORD_TOKEN = input('Enter your Discord API token: ')
     DISCORD_CHANNEL = input('Enter your Discord Channel ID: ')
     RIOT_KEY = input('Enter your Riot API key: ')
-    SUMMONER_NAME = input('Enter the desired summoner to track: ')
     DIFF = input('Enter necessary kill-death difference: ')
 
-    # Getting Encrypted Account ID
-    response = requests.get(url='https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + SUMMONER_NAME + '?api_key=' + RIOT_KEY)
-    account_info = json.loads(response.text)
-    ACCOUNT_ID = account_info['accountId']
+    # Getting Summoner Names
+    while True:
+        num = int(input('Enter how many people to track: '))
+        if num < 0:
+            print('Please enter a nonnegative integer')
+            continue
+        NUM_OF_INTERS = num
+        break
+    for i in range(NUM_OF_INTERS):
+        summoner = input(f'Enter summoner #{i + 1} name: ')
+        summoners_names_list.append(summoner)
 
-    # API Call
-    response = requests.get(url='https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/aAX05Uf5lrObicds4lcyq7El4rOGJyPUUxu2vu7_5wqoog?endIndex=1&beginIndex=0&api_key=' + RIOT_KEY)
-    most_recent_match = json.loads(response.text)
-    game_id = most_recent_match['matches'][0]['gameId']
+    # Retrieving Encyrpted IDs
+    index = 0
+    for s in summoners_names_list:
+        response = requests.get(url='https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + s + '?api_key=' + RIOT_KEY)
+        account_info = json.loads(response.text)
+        sumId = account_info['accountId']
+        response = requests.get(url='https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + sumId + '?endIndex=1&beginIndex=0&api_key=' + RIOT_KEY)
+        most_recent_match = json.loads(response.text)
+        game_id = most_recent_match['matches'][0]['gameId']
+        newSum = Summoner(index, s, sumId, game_id)
+        print("NEWSUM: " + str(newSum.encrypted_id))
+        summoners_list.append(newSum)
+        index = index + 1
 
+    # Dumping list of summoner objects to pickle file
+    with open('summoners.pkl', 'wb') as output:
+        pickle.dump(len(summoners_list), output, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(summoners_list, output, pickle.HIGHEST_PROTOCOL)
+
+    # Writing .env file
     with open('.env', 'w') as file:
         file.write('# .env\n\n')
         file.write('# Discord Variables\n')
@@ -35,38 +62,9 @@ def main():
         file.write('\n')
         file.write('# Riot Variables\n')
         file.write(f'RIOT_KEY="{RIOT_KEY}"\n')
-        file.write(f'ACCOUNT_NO="{ACCOUNT_ID}"\n')
-        file.write(f'LAST_GAME="{str(game_id)}"\n')
         file.write('\n')
         file.write('# App Variables\n')
         file.write(f'DIFF="{DIFF}"\n')
-
-
-    # Leaderboard
-    print('Answers to the following questions will be to implement a leaderboard.')
-    while True:
-        num = int(input('Enter how many people to track: '))
-        if num < 0:
-            print('Please enter a nonnegative integer')
-            continue
-        NUM_OF_INTERS = num
-        break
-    summoners_names_list = []
-    for i in range(NUM_OF_INTERS):
-        summoner = input(f'Enter summoner #{i + 1} name: ')
-        summoners_names_list.append(summoner)
-
-    summoners_list = []
-    for s in summoners_names_list:
-        response = requests.get(url='https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + s + '?api_key=' + RIOT_KEY)
-        account_info = json.loads(response.text)
-        sumId = account_info['accountId']
-        newSum = Summoner(s, sumId)
-        summoners_list.append(newSum)
-
-    with open('../leaderboard_summoners.pkl', 'wb') as output:
-        pickle.dump(len(summoners_list), output, pickle.HIGHEST_PROTOCOL)
-        pickle.dump(summoners_list, output, pickle.HIGHEST_PROTOCOL)
 
 if __name__ == "__main__":
     main()
