caf4c7ed4c30f5f8075acba8819ee0d838305aa0
briankoehler <briankoehler@ufl.edu>
2020-12-25 20:13:23 -0500

    Basic database implementation

--- bot/database.py
+++ bot/database.py
@@ -8,88 +8,128 @@ def create_database():
     conn = sqlite3.connect('jit.db')
     
     
-def create_int_table():
+def create_match_table():
     conn = sqlite3.connect('jit.db')
     c = conn.cursor()
-    
-    c.execute('''
-              CREATE TABLE ints(
-                  guild_id TEXT NOT NULL,
-                  match_id TEXT NOT NULL,
-                  champ TEXT NOT NULL,
-                  summoner_name TEXT NOT NULL,
-                  kills INTEGER CHECK(kills > -1),
-                  deaths INTEGER CHECK(deaths > -1),
-                  assists INTEGER CHECK(deaths > -1)
-              );
-              ''')
-    
-    
-def add_int(guild_id, new_match):
-    conn = sqlite3.connect('jit.db')
-    c = conn.cursor()
-    
-    data = (guild_id, new_match.game_id, new_match.champ, new_match.summoner, new_match.kills, new_match.deaths, new_match.assists)
     
     try:
-        c.execute(f''' 
-                  INSERT INTO ints
-                  VALUES (?, ?, ?, ?, ?, ?, ?);
-                  ''', data)
-        
-        conn.commit()
+        c.execute('''
+                CREATE TABLE match(
+                    guild TEXT NOT NULL,
+                    id TEXT NOT NULL,
+                    duration INTEGER CHECK(duration > -1),
+                    summoner TEXT NOT NULL,
+                    champ TEXT NOT NULL,
+                    kills INTEGER CHECK(kills > -1),
+                    deaths INTEGER CHECK(deaths > -1),
+                    assists INTEGER CHECK(deaths > -1)
+                );
+                ''')
         return True
+    
     except:
         return False
     
     
-def get_top_ints(guild_id):
+def add_match(guild, match):
     conn = sqlite3.connect('jit.db')
     c = conn.cursor()
     
-    c.execute('''
-              WITH guild_ints AS (SELECT * FROM ints WHERE guild_id == ? ORDER BY deaths DESC)
-              
-              SELECT * FROM guild_ints LIMIT 10;
-              ''', (guild_id,))
+    data = (guild, match.id, match.duration, match.summoner, match.champ, match.kills, match.deaths, match.assists)
     
-    top_ints = c.fetchall()
-    return top_ints
+    try:
+        c.execute(f''' 
+                  INSERT INTO int
+                  VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+                  ''', data)
+        
+        conn.commit()
+        return True
+    
+    except:
+        return False
+    
+    
+def get_top_ints(guild):
+    conn = sqlite3.connect('jit.db')
+    c = conn.cursor()
+    
+    try:
+        c.execute('''
+                WITH t1 AS (SELECT * FROM int WHERE guild == ? ORDER BY deaths DESC)
+                
+                SELECT * FROM t1 LIMIT 10;
+                ''', (guild,))
+        
+        top_ints = c.fetchall()
+        return top_ints
+    
+    except:
+        return []
 
 
 def create_summoners_table():
     conn = sqlite3.connect('jit.db')
     c = conn.cursor()
     
-    c.execute('''
-              CREATE TABLE summoners(
-                  encrypted_id TEXT PRIMARY KEY,
-                  name TEXT NOT NULL,
-                  last_game_id TEXT NOT NULL,
-                  guild_id TEXT NOT NULL
-              );
-              ''')
+    try:
+        c.execute('''
+                CREATE TABLE summoner(
+                    guild TEXT NOT NULL,
+                    id TEXT PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    last_match TEXT NOT NULL
+                );
+                ''')
+        return True
+        
+    except:
+        return False
     
     
-def add_summoner(guild_id, summoner):
+def add_summoner(guild, summoner):
     conn = sqlite3.connect('jit.db')
     c = conn.cursor()
     
-    data = (summoner.encrypted_id, summoner.name, summoner.last_game_id, guild_id)
+    data = (guild, summoner.id, summoner.name, summoner.last_match)
     
-    c.execute('''
-              INSERT INTO summoners
-              VALUES (?, ?, ?);
-              ''', data)
+    try:
+        c.execute('''
+                INSERT INTO summoner
+                VALUES (?, ?, ?, ?);
+                ''', data)
+        return True
+        
+    except:
+        return False
     
     
-def get_summoners(guild_id):
+def get_summoners(guild):
     conn = sqlite3.connect('jit.db')
     c = conn.cursor()
     
-    c.execute('''
-              SELECT * FROM summoners WHERE guild_id = ?
-              ''', (guild_id,))
+    try:
+        c.execute('''
+                SELECT * FROM summoner WHERE guild = ?
+                ''', (guild,))
+        
+        summoners = c.fetchall()
+        return summoners
     
-    summoners = c.fetchall()
-    return summoners
+    except:
+        return []
+
+def remove_summoner(guild, name):
+    conn = sqlite3.connect('jit.db')
+    c = conn.cursor()
+    
+    data = (guild, name)
+    
+    try:
+        c.execute('''
+                  DELETE FROM summoner WHERE guild = ? and name = ?
+                  ''', data)
+        return True
+        
+    finally:
+        return False
